50fe6f84237c349d26784e12a0b7c2d93321f874
zhengxyz123 <3422794023@qq.com>
2024-08-21 17:20:03 +0800

    Using English README.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # num2str
-正如它的名字所描述的那样，这是一个没有什么含金量的、默默无闻的python库。
+**num2str** is a tiny Python library to convert a floating number into some specific mathematical expressions. The algorithm, which can be used in numerical calculation tools, is simple and efficient.
 
-它的功能不太好描述，那我们先看演示吧：
+The following example shows how to use this library:
 ```python
 >>> from math import *
 >>> from num2str import num2str
@@ -15,13 +15,11 @@
 'sqrt(2)+sqrt(3)'
 ```
 
-就是这样，它能将浮点数转换为数学表达式：
+It supports the following formats:
 
-1. 分子、分母都为整数的分数
-2. `±sqrt(a)/b`型的数（a、b是整数）
-3. `(±sqrt(a)±sqrt(b))/c`型的数（a、b、c都是整数）
-4. 如上述`(1)`或`(2)`型的数乘以π
+1. $\frac{a}{b}$
+2. $\frac{\sqrt{a}}{b}$
+3. $\frac{\sqrt{a}+\sqrt{b}}{c}$
+4. $\text{some value}\times\pi$
 
-另外的就没办法啦qwq
-
-鄙人还在`num2sqrts`目录下详细讲解了那个不明所以的`_num2sqrts`函数的原理。
+We use a new algorithm to calculate the (3) format, You can find more information about the algorithm in [this blog](https://zhengxyz123.github.io/coding/num2sqrts).
--- helper.py
+++ helper.py
@@ -10,13 +10,11 @@ def _fsqrt(n: float) -> float:
 def normal_one(n: float) -> tuple[int, int] | None:
     for x in range(101):
         for y in range(101):
-            if math.isclose(_fsqrt(x) + _fsqrt(y), n, rel_tol=1e-16):
+            if math.isclose(_fsqrt(x) + _fsqrt(y), n, rel_tol=1e-12):
                 return x, y
 
 
-def num2sqrts(
-    n: int | float, max_num: int = 1000, count_loops: bool = False
-) -> int | tuple[int, int] | None:
+def num2sqrts(n: int | float, max_num: int = 1000) -> int:
     if n >= 0:
         mid = math.floor((n / 2) ** 2) + 0.5
     else:
@@ -30,15 +28,9 @@ def num2sqrts(
         b = actual_mid - d
         b = _fsqrt(math.copysign(round(b**2), b))
         if abs(a**2) > max_num or abs(b**2) > max_num:
-            if count_loops:
-                return loops
-            return
+            return loops
         if math.isclose(a + b, n, rel_tol=1e-12):
-            if count_loops:
-                return loops
-            return int(round(math.copysign(a**2, a))), int(
-                round(math.copysign(b**2, b))
-            )
+            return loops
         loops += 1
         t += 1
 
@@ -66,8 +58,12 @@ def compare():
 
 
 def perform():
-    import numpy as np
-    import matplotlib.pyplot as plt
+    try:
+        import numpy as np
+        import matplotlib.pyplot as plt
+    except ModuleNotFoundError:
+        print("numpy and matplotlib is not installed!")
+        exit(1)
 
     mat = np.zeros((201, 201))
     for x in range(-100, 101):
@@ -75,22 +71,23 @@ def perform():
             if x == y:
                 mat[x + 100][-y + 100] = 1
             else:
-                mat[x + 100][-y + 100] = num2sqrts(
-                    _fsqrt(x) + _fsqrt(y), count_loops=True
-                )
+                mat[x + 100][-y + 100] = num2sqrts(_fsqrt(x) + _fsqrt(y))
     fig, ax = plt.subplots()
     img = ax.matshow(mat)
     fig.colorbar(img, ax=ax)
     ax.set_xlabel("x")
-    ax.xaxis.set_major_formatter(lambda x, pos: int(x - 100))
+    ax.xaxis.set_major_formatter(lambda x, pos: str(int(x - 100)))
     ax.set_ylabel("y")
-    ax.yaxis.set_major_formatter(lambda y, pos: int(-y + 100))
+    ax.yaxis.set_major_formatter(lambda y, pos: str(int(-y + 100)))
     fig.savefig("perform.pdf")
 
 
 if __name__ == "__main__":
     from sys import argv
-
+    
+    if len(argv) < 2:
+        print("need one argument!")
+        exit(1)
     if argv[1] == "perform":
         perform()
     elif argv[1] == "compare":
